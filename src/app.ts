155d15f0f44313a2233f0a43f71a3ba1a25581a8
40213151 <daimarukeiichirou@daimarukeiichirounoMacBook-Pro.local>
2026-01-02 22:02:50 +0900

    エントリーポイントの実装

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
+import { BookController } from "./presentation/bookController";
 
 const app = express();
 
 app.use(express.json());
 
+const bookController = new BookController();
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.json({ message: "HW" });
-});
+app.post("/books", booksController.add.bind(booksController));
+app.get("/books/:id", booksController.findById.bind(booksController));
 
-app.listen(PORT, () => console.log("SIR"));
+app.listen(PORT, () => console.log("server is running"));
