a6df7f15d636997b7eb358ee57cadf3d02241579
40213151 <daimarukeiichirou@daimarukeiichirounoMacBook-Pro.local>
2026-01-02 22:14:45 +0900

    修正

--- src/dataAccess/prismaBoolRepository.ts
+++ src/dataAccess/prismaBoolRepository.ts
@@ -1,7 +1,7 @@
 import { Book, PrismaClient } from "@prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
-const adapter = new PrismaBetterSqlite3({ url: "file:./prisma/dev.db" });
+const adapter = new PrismaBetterSqlite3({ url: "file:./dev.db" });
 
 export class PrismaBookRepository {
   private prisma: PrismaClient;
